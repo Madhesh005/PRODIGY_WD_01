b92e36abd43adc1501b75f6ca7b595963e541199
Madhesh005 <madheshr005@gmail.com>
2024-07-16 20:39:09 +0530

    Landing Page Created

---
+++ script.js
@@ -0,0 +1,13 @@
+var sidenavigationbar = document.querySelector(".sidenavbar");
+
+function sidenavigation(){
+    sidenavigationbar.style.left = "0%";
+}
+
+function closenavbar(){
+    sidenavigationbar.style.left = "-60%";
+}
+
+function alerting(){
+    alert("We'll Contact you soon...");
+}
